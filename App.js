d2920698db2752a64e734f3c0997eab925dd5864
frontend <gabriellennon@wizsolucoes.com.br>
2022-06-20 22:34:54 -0300

    adjustments in libs

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
+// @refresh reset
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC90tWGxe2sW0HhPrPut2HKQwih1LxTjzc",
+//   authDomain: "react-native-chat-d45da.firebaseapp.com",
+//   projectId: "react-native-chat-d45da",
+//   storageBucket: "react-native-chat-d45da.appspot.com",
+//   messagingSenderId: "298456423380",
+//   appId: "1:298456423380:web:224688b81626e8f1fc1441"
+// };
 
 export default function App() {
   return (
